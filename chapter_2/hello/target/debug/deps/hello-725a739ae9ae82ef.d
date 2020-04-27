51c7d1aa51cf3c70450c323a7d878ab495d1f41b
yamagata-akita <tiyduts@gmail.com>
2020-04-27 20:52:33 +0900

    hello world

---
+++ chapter_2/hello/target/debug/deps/hello-725a739ae9ae82ef.d
@@ -0,0 +1,5 @@
+/home/koji/rust_tutorial/chapter_2/hello/target/debug/deps/hello-725a739ae9ae82ef: src/main.rs
+
+/home/koji/rust_tutorial/chapter_2/hello/target/debug/deps/hello-725a739ae9ae82ef.d: src/main.rs
+
+src/main.rs:
---
+++ chapter_2/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/koji/rust_tutorial/chapter_2/hello/target/debug/hello: /home/koji/rust_tutorial/chapter_2/hello/src/main.rs
