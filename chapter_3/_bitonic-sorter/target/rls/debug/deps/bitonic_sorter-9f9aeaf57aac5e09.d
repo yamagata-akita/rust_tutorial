5176733b9b51904779f88afa7448b0160c8a8595
yamagata-akita <57021786+yamagata-akita@users.noreply.github.com>
2021-04-25 13:32:38 +0900

    delete target directory

--- chapter_3/_bitonic-sorter/target/rls/debug/deps/bitonic_sorter-9f9aeaf57aac5e09.d
+++
@@ -1,5 +0,0 @@
-/home/koji/rust_tutorial/chapter_3/bitonic-sorter/target/rls/debug/deps/bitonic_sorter-9f9aeaf57aac5e09.rmeta: src/main.rs
-
-/home/koji/rust_tutorial/chapter_3/bitonic-sorter/target/rls/debug/deps/bitonic_sorter-9f9aeaf57aac5e09.d: src/main.rs
-
-src/main.rs:
